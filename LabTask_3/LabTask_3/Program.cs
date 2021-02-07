39ba619af1b9bd2d6f8e485f43a14287fc3c2119
AAMamnun <aamamnun1998@gmail.com>
2021-02-07 22:40:00 +0600

    Lab task 3 student

---
+++ LabTask_3/LabTask_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student s = new Student("Abdullah Mamnun", "18-38257-2", "CSE", (float)3.44);
+            s.Name = "Mamnun";
+            s.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabTask_3/LabTask_3/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask_3
+{
+    class Student
+    {
+
+        private String name;
+        private String id;
+        private String deparment;
+        private float cgpa;
+
+
+        public Student(String name, String id, String deparment, float cgpa)
+
+        {
+            this.name = name;
+            this.id = id;
+            this.deparment = deparment;
+            this.cgpa = cgpa;
+        }
+
+        public void ShowInfo()
+
+        {
+            Console.WriteLine("Name :" + name);
+            Console.WriteLine("Id :" + id);
+            Console.WriteLine("Deparment :" + deparment);
+            Console.WriteLine("Cgpa :" + cgpa);
+
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Department
+        {
+            get { return deparment; }
+            set { deparment = value; }
+        }
+
+        public float Cgpa
+        {
+            get { return cgpa; }
+            set { cgpa = value; }
+        }
+    }
+}
