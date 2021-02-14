a79a9ff7180e35ad4a1601a07f888d3046ae32d2
AAMamnun <aamamnun1998@gmail.com>
2021-02-14 15:19:44 +0600

    lab 4 task 3

---
+++ Lab_4_task_3/Lab_4_task_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4,5,1,2 };
+
+            int single = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+
+                {
+                    if (arr[i]  == arr[j])
+
+                        single++;
+
+                }
+
+
+
+            }
+
+            Console.WriteLine("Single element{0}", single);
+            Console.ReadKey();
+
+        }
+    }
+}
